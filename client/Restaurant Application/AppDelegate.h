ec9f4f402d8c769f0d87639c32351761c72d38e4
Ömer Aslan <omer.aslan@ozu.edu.tr>
2015-08-03 08:08:13 +0300

    Added Initialized Restaurant application with pods

---
+++ client/Restaurant Application/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Restaurant Application
+//
+//  Created by Ömer Emre Aslan on 03/08/15.
+//  Copyright © 2015 Ömer Emre Aslan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
