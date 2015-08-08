91f3a77dc03a77e5f31576be707fb866998b07e7
Ömer Aslan <omer.aslan@ozu.edu.tr>
2015-08-08 22:46:49 +0300

    Initialized UserManager

---
+++ client/Restaurant Application/Managers/UserManager/UserManager.h
@@ -0,0 +1,13 @@
+//
+//  UserManager.h
+//  Restaurant Application
+//
+//  Created by Ömer Emre Aslan on 07/08/15.
+//  Copyright © 2015 Ömer Emre Aslan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserManager : NSObject
+
+@end
---
+++ client/Restaurant Application/Managers/UserManager/UserManager.m
@@ -0,0 +1,13 @@
+//
+//  UserManager.m
+//  Restaurant Application
+//
+//  Created by Ömer Emre Aslan on 07/08/15.
+//  Copyright © 2015 Ömer Emre Aslan. All rights reserved.
+//
+
+#import "UserManager.h"
+
+@implementation UserManager
+
+@end
